5babb7948ef21fe560637603df6447913848ce8b
Vladimir Lunkin <vova.gold.201427@gmail.com>
2020-10-14 23:02:50 +0300

    added logic tests

--- project/tests/test2.cpp
+++ project/tests/test2.cpp
@@ -6,14 +6,62 @@ extern "C" {
 #include "music.h"
 }
 
-void track_cmp(music_track &track1, music_track &track2) {
+void cmp_track(music_track &track1, music_track &track2) {
     ASSERT_STREQ(track1.author, track2.author);
     ASSERT_STREQ(track1.performer, track2.performer);
     ASSERT_STREQ(track1.name, track2.name);
     ASSERT_EQ(track1.duration, track2.duration);
 }
 
-TEST(logic_test, read_track_info) {
+void cmp_file(const char *path_output_file, const char *path_result_file) {
+    FILE *fptr_out = fopen(path_output_file, "rt");
+    FILE *fptr_res = fopen(path_result_file, "rt");
+    if (fptr_out == nullptr || fptr_res == nullptr) {
+        FAIL();
+    }
+
+    size_t num1;
+    size_t num2;
+    if (fscanf(fptr_out, "%ld", &num1) != 1 ||
+        fscanf(fptr_res, "%ld", &num2) != 1 ||
+        num1 != num2) {
+        fclose(fptr_out);
+        fclose(fptr_res);
+        FAIL();
+    }
+    for (size_t i = 0; i < num1; ++i) {
+        music_track track1;
+        music_track track2;
+        if (read_track_info(fptr_out, &track1) ||
+            read_track_info(fptr_res, &track2)) {
+            free_track(&track1);
+            free_track(&track2);
+            fclose(fptr_out);
+            fclose(fptr_res);
+            FAIL();
+        }
+        cmp_track(track1, track2);
+        free_track(&track1);
+        free_track(&track2);
+    }
+
+    fclose(fptr_out);
+    fclose(fptr_res);
+}
+
+void test_main(const char *path_input_file, const char *path_output_file, const char *author, const char *path_result_file) {
+    size_t num = 0;
+    music_track *track_library = create_track_library(path_input_file, &num);
+    if (track_library == nullptr) {
+        FAIL();
+    }
+    print_tracks_by_author(path_output_file, track_library, num, author);
+    free_track_library(track_library, num);
+
+    cmp_file(path_output_file, path_result_file);
+}
+
+TEST(test_func, read_track_info) {
     const char *path_input_file = "../tests/data/track_info";
     FILE *fptr = fopen(path_input_file, "rt");
     if (fptr == nullptr) {
@@ -27,13 +75,13 @@ TEST(logic_test, read_track_info) {
         FAIL();
     }
 
-    track_cmp(track1, track2);
+    cmp_track(track1, track2);
 
     free_track(&track2);
     fclose(fptr);
 }
 
-TEST(logic_test, print_track_info) {
+TEST(test_func, print_track_info) {
     const char *path_output_file = "../tests/tmp/print_track_info/test1";
     FILE *fptr = fopen(path_output_file, "w+");
     if (fptr == nullptr) {
@@ -51,66 +99,53 @@ TEST(logic_test, print_track_info) {
         FAIL();
     }
 
-    track_cmp(track1, track2);
+    cmp_track(track1, track2);
 
     free_track(&track2);
     fclose(fptr);
 }
 
-TEST(logic_test, create_track_library) {
-    char path_input_file[] = "../tests/data/libx";
-    char path_output_file[] = "../tests/tmp/create_track_library/testx";
-    char path_result_file[] = "../tests/result/create_track_library/testx";
-    char *authors[] = {"Хаски", "a"};
+TEST(logic_test, test1) {
+    const char *path_input_file = "../tests/data/lib1";
+    const char *path_output_file = "../tests/tmp/logic_test/test1";
+    const char *path_result_file = "../tests/result/logic_test/test1";
+    const char *authors = "Хаски";
 
-    char num_test;
-    size_t k;
-    for (num_test = '1', k = 0; k < 2; ++num_test, ++k) {
-        path_input_file[strlen(path_input_file) - 1] = num_test;
-        path_output_file[strlen(path_output_file) - 1] = num_test;
-        path_result_file[strlen(path_result_file) - 1] = num_test;
-        const char *author = authors[k];
+    test_main(path_input_file, path_output_file, authors, path_result_file);
+}
 
-        size_t num = 0;
-        music_track *track_library = create_track_library(path_input_file, &num);
-        if (track_library == nullptr) {
-            FAIL();
-        }
-        print_tracks_by_author(path_output_file, track_library, num, author);
-        free_track_library(track_library, num);
+TEST(logic_test, test2) {
+    const char *path_input_file = "../tests/data/lib1";
+    const char *path_output_file = "../tests/tmp/logic_test/test2";
+    const char *path_result_file = "../tests/result/logic_test/test2";
+    const char *authors = "";
 
-        FILE *fptr_out = fopen(path_output_file, "rt");
-        FILE *fptr_res = fopen(path_result_file, "rt");
-        if (fptr_out == nullptr || fptr_res == nullptr) {
-            FAIL();
-        }
+    test_main(path_input_file, path_output_file, authors, path_result_file);
+}
 
-        size_t num1;
-        size_t num2;
-        if (fscanf(fptr_out, "%ld", &num1) != 1 ||
-            fscanf(fptr_res, "%ld", &num2) != 1 ||
-            num1 != num2) {
-            fclose(fptr_out);
-            fclose(fptr_res);
-            FAIL();
-        }
-        for (size_t i = 0; i < num1; ++i) {
-            music_track track1;
-            music_track track2;
-            if (read_track_info(fptr_out, &track1) ||
-                read_track_info(fptr_res, &track2)) {
-                free_track(&track1);
-                free_track(&track2);
-                fclose(fptr_out);
-                fclose(fptr_res);
-                FAIL();
-            }
-            track_cmp(track1, track2);
-            free_track(&track1);
-            free_track(&track2);
-        }
+TEST(logic_test, test3) {
+    const char *path_input_file = "../tests/data/lib2";
+    const char *path_output_file = "../tests/tmp/logic_test/test3";
+    const char *path_result_file = "../tests/result/logic_test/test3";
+    const char *authors = "a";
 
-        fclose(fptr_out);
-        fclose(fptr_res);
-    }
+    test_main(path_input_file, path_output_file, authors, path_result_file);
+}
+
+TEST(logic_test, test4) {
+    const char *path_input_file = "../tests/data/lib2";
+    const char *path_output_file = "../tests/tmp/logic_test/test4";
+    const char *path_result_file = "../tests/result/logic_test/test4";
+    const char *authors = "AAAAAAAAAA";
+
+    test_main(path_input_file, path_output_file, authors, path_result_file);
+}
+
+TEST(logic_test, test5) {
+    const char *path_input_file = "../tests/data/lib2";
+    const char *path_output_file = "../tests/tmp/logic_test/test5";
+    const char *path_result_file = "../tests/result/logic_test/test5";
+    const char *authors = "no_author";
+
+    test_main(path_input_file, path_output_file, authors, path_result_file);
 }
